407a58cb48d031eb62dc33fd7de9810c133752a9
Anna Peterson <peterson.anna.c@gmail.com>
2018-09-26 16:43:55 -0700

    Add style and placeholders to shopping cart

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -5,7 +5,11 @@ import itemList from '../../database/itemList.js';
 const shoppingCart = (props) => {
   return (
     <div className={classes.ShoppingCart}>
-      <p>ShoppingCart!</p>
+      <p>ShoppingCart</p>
+      <p>(button) X</p>
+      <p>(insert MenuCards)</p>
+      <p>(button) Checkout Now</p>
+      <p>(button) Close</p>
     </div>
   );
 };
