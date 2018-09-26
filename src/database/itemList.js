3c654fb5dc44e4f6445547a110f329b40c74130a
Anna Peterson <peterson.anna.c@gmail.com>
2018-09-26 13:12:32 -0700

    Create database file

---
+++ src/database/itemList.js
@@ -0,0 +1,23 @@
+
+const itemList = [
+  {
+    name: "Baseball",
+  },
+  {
+    name: "Tennis Ball",
+  },
+  {
+    name: "Football",
+  },
+  {
+    name: "Basketball",
+  },
+  {
+    name: "American Football",
+  },
+  {
+    name: "Ping Pong Ball",
+  },
+];
+
+export default itemList;
