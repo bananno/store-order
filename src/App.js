60a29d108c58c2b7eefb205e7f095a2846abbc3c
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2018-09-26 12:51:28 -0700

    Layout - Add the layout component to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Layout />
         <h1>Sports app</h1>
       </div>
     );
