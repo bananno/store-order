30a725b2a1f689e0b7be3e456728fee8c7971daf
Anna Peterson <peterson.anna.c@gmail.com>
2018-09-26 13:12:47 -0700

    Import database to menu and shopping cart components

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Menu.css';
+import itemList from '../../database/itemList.js';
 
 const Menu = (props) => {
   return <p>Menu!</p>;
--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './ShoppingCart.css';
+import itemList from '../../database/itemList.js';
 
 const ShoppingCart = (props) => {
   return <p>ShoppingCart!</p>;
