e404974c5398716adc39c4310acbfa8aa3f3a754
Anna Peterson <peterson.anna.c@gmail.com>
2018-09-26 14:32:38 -0700

    Add button to toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './Toolbar.css';
+import ShoppingCartButton from './ShoppingCartButton/ShoppingCartButton';
 
 const toolbar = (props) => {
   return (
     <div className={classes.Toolbar}>
       <p>Toolbar!</p>
+      <ShoppingCartButton/>
     </div>
   );
 };
