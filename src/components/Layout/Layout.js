b60b150feefe2a71c4ca3b04da5262eb0b530184
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2018-10-01 14:22:58 -0700

    Convert layout component to a class

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.css';
 import Toolbar from '../Toolbar/Toolbar';
 import Menu from '../Menu/Menu';
 import ShoppingCart from '../ShoppingCart/ShoppingCart';
 import Backdrop from '../UI/Backdrop/Backdrop';
 
-const layout = (props) => {
-  return (
-    <div className={classes.Layout}>
+class Layout extends Component {
+  render() {
+    return(
+      <div className={classes.Layout}>
       <Toolbar />
       <Menu />
       <ShoppingCart />
       <Backdrop />
     </div>
-  );
-};
+    )
+  }
+}
 
-export default layout;
+export default Layout;
 
