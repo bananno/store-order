deb9e97a594ab5e0786f8935d979323dccfaf7ed
Anna Peterson <peterson.anna.c@gmail.com>
2018-10-01 14:37:05 -0700

    Add menu cards to shopping cart

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import classes from './ShoppingCart.css';
 import itemList from '../../database/itemList.js';
+import MenuCard from '../MenuCard/MenuCard';
 
 const shoppingCart = (props) => {
   return (
     <div className={classes.ShoppingCart}>
       <p>ShoppingCart</p>
       <p>(button) X</p>
-      <p>(insert MenuCards)</p>
+
+      {itemList.map((items, i) => (
+        <MenuCard name={items.name} img={items.img} key={i}/>
+      ))}
+
       <p>(button) Checkout Now</p>
       <p>(button) Close</p>
     </div>
