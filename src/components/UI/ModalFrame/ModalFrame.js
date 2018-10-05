79242fe879db0ddda1d48ca4f8c9a6db3455d931
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2018-10-04 19:01:12 -0700

    Remove comments

--- src/components/UI/ModalFrame/ModalFrame.js
+++ src/components/UI/ModalFrame/ModalFrame.js
@@ -7,7 +7,6 @@ const modalFrame = (props) => {
   }
 
   return (
-    // <div className={props.test ? classes.ModalFrame : classes.Frame}>
     <div className={classes.ModalFrame}>
       {props.children}
     </div>
@@ -15,6 +14,3 @@ const modalFrame = (props) => {
 };
 
 export default modalFrame;
-
-
-// className={classes.Button + (props.quantity == 0 ? (" " + classes.ButtenWhenZero) : "")}>-</button>
